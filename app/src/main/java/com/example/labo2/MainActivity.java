1fdc7cc112f0a0c889821b181e8a61f8a98a1de8
Cristian Mundo <00086917@uca.edu.sv>
2019-03-15 09:57:50 -0600

    first commit

---
+++ app/src/main/java/com/example/labo2/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.labo2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
+
+    EditText user, pass;
+    Button button;
+    TextView caja;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        user = findViewById(R.id.user);
+        pass = findViewById(R.id.pass);
+        button = findViewById(R.id.button);
+        caja = findViewById(R.id.cajaEmpty);
+
+        button.setOnClickListener(this);
+        button.setOnLongClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        caja.setText(user.getText());
+
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        caja.setText(pass.getText());
+        return false;
+    }
+}
+
